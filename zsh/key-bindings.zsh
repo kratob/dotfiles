6f401ddfb971a2e812a38129332f15eee58c9c62
Tobias Kraze <tobias.kraze@makandra.de>
2011-07-18 11:07:26 +0200

    teak for zsh handling of special keys

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -1,6 +1,6 @@
 bindkey -v
 
-source ~/.zkbd/$TERM-${DISPLAY:-$VENDOR-$OSTYPE}
+source ~/.zkbd/$TERM
 [[ -n ${key[Left]} ]] && bindkey "${key[Left]}" backward-char
 [[ -n ${key[Right]} ]] && bindkey "${key[Right]}" forward-char
 [[ -n "${key[Home]}"    ]]  && bindkey  "${key[Home]}"    beginning-of-line
