db0c979214cfe717c6872dc8a3f7b67c6420d2ad
Tobias Kraze <tobias@kraze.eu>
2011-07-23 23:16:56 +0200

    better git prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -6,20 +6,55 @@ git_branch() {
   echo $(/usr/bin/git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
 }
 
-git_dirty() {
-  st=$(/usr/bin/git status 2>/dev/null | tail -n 1)
-  if [[ $st == "" ]]
-  then
-    echo ""
-  else
-    if [[ $st == "nothing to commit (working directory clean)" ]]
-    then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
+git_prompt() {
+  local git_dir branch state st info merge_branch
+  state=""
+  git_dir=$(/usr/bin/git rev-parse --git-dir 2> /dev/null) || return
+
+  if test -d "$git_dir/rebase-apply"; then
+    if test -f "$git_dir/rebase-apply/rebasing"; then
+      state="rebase"
+    elif test -f "$git_dir/rebase-apply/applying"; then
+      state="am"
     else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
+      state="am/rebase"
     fi
+    branch="$(cat $git_dir/rebase-apply/head-name 2>/dev/null)"
+  elif test -f "$git_dir/rebase-merge/interactive"; then
+    state="rebase -i"
+    branch="$(cat "$git_dir/rebase-merge/head-name")"
+  elif test -d "$git_dir/rebase-merge"; then
+    state="rebase -m"
+    branch="$(cat "$git_dir/rebase-merge/head-name")"
+  elif test -f "$git_dir/MERGE_HEAD"; then
+    merge_branch="$(git describe `cat $git_dir/MERGE_HEAD` --all 2>/dev/null)"
+    state="merge with ${merge_branch#heads/}"
+    branch="$(git symbolic-ref HEAD 2>/dev/null)"
+  else
+    if test -f "$git_dir/BISECT_LOG"; then
+      state="bisect"
+    fi
+    branch="$(git symbolic-ref HEAD 2>/dev/null)" || \
+      branch="$(git describe --exact-match HEAD 2>/dev/null)" || \
+      branch="$(cut -c1-7 "$git_dir/HEAD")..."
+  fi
+
+  if [[ $state == "" ]] 
+  then
+    info="${branch#refs/heads/}"
+  else
+    info="${branch#refs/heads/} [$state]"
+  fi
+
+  st=$(/usr/bin/git status 2>/dev/null | tail -n 1)
+  if [[ $st == "nothing to commit (working directory clean)" ]]
+  then
+    echo "on %{$fg_bold[green]%}$info%{$reset_color%}"
+  else
+    echo "on %{$fg_bold[red]%}$info%{$reset_color%}"
   fi
 }
+
 
 git_prompt_info () {
  ref=$(/usr/bin/git symbolic-ref HEAD 2>/dev/null) || return
@@ -35,16 +70,10 @@ remote_ref () {
   /usr/bin/git show-ref origin/$(git_branch) --hash 2>/dev/null
 }
 
-unpushed () {
-  /usr/bin/git cherry -v origin/$(git_branch) 2>/dev/null
-}
-
 need_push () {
-  if [[ $(local_ref) == $(remote_ref) ]]
+  if [[ $(local_ref) != $(remote_ref) ]]
   then
-    echo " "
-  else
-    echo " %{$fg_bold[cyan]%}(unpushed)%{$reset_color%} "
+    echo "%{$fg_bold[cyan]%}(unpushed)%{$reset_color%}"
   fi
 }
 
@@ -72,12 +101,8 @@ directory_name(){
 }
 
 
-export PROMPT=$'\n$(directory_name) $(git_dirty)$(need_push)\n› '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[grey]%}$(todo)%{$reset_color%}"
-}
+export PROMPT=$'\n$(directory_name) $(git_prompt) $(need_push)\n› '
 
 precmd() {
   title "zsh" "%m" "%55<...<%~"
-  #set_prompt
 }
