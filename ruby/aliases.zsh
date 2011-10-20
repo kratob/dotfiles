f6b6525b59840a3ced836adfa6b89e327f8e7b8c
Tobias Kraze <tobias@kraze.eu>
2011-10-20 13:56:02 +0200

    tweak ruby aliases: rdbm and pryc

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -8,12 +8,12 @@ function _rails_command () {
   fi
 }
 
-alias pryc='pry -r./config/environment.rb'
+alias pryc='pry -r./config/environment.rb -rrails/console/app'
 alias rc='_rails_command console'
 alias rg='_rails_command generate'
 alias rd='_rails_command destroy'
 alias rdb='_rails_command dbconsole'
-alias rdbm='rake db:migrate db:test:clone'
+alias rdbm='b rake db:migrate db:test:clone'
 alias rp='_rails_command plugin'
 alias devlog='tail -f log/development.log'
 alias rrs='touch tmp/restart.txt'
