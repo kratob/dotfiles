cad7e2e0a43e7814fd04df09c64c77b5421c6b3c
Tobias Kraze <tobias@kraze.eu>
2011-10-20 13:54:52 +0200

    remove xcompmgr

--- awesome/.config/awesome.symlink/rc.lua
+++ awesome/.config/awesome.symlink/rc.lua
@@ -357,6 +357,3 @@ end)
 client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
-
-awful.util.spawn("nautilus -n")
-awful.util.spawn("xcompmgr -as &")
