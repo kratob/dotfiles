1e860672570d99d85a59c587bb332711831878eb
Tobias Kraze <tobias@kraze.eu>
2012-06-15 11:15:50 +0200

    vim: update and monkey-patch command-t

--- vim/.vim.symlink/ruby/command-t/match_window.rb
+++ vim/.vim.symlink/ruby/command-t/match_window.rb
@@ -38,8 +38,9 @@ module CommandT
       # save existing window dimensions so we can restore them later
       @windows = []
       (0..(::VIM::Window.count - 1)).each do |i|
-        window = OpenStruct.new :index => i, :height => ::VIM::Window[i].height
-        @windows << window
+        @windows << OpenStruct.new(:index   => i,
+                                   :height  => ::VIM::Window[i].height,
+                                   :width   => ::VIM::Window[i].width)
       end
 
       # global settings (must manually save and restore)
@@ -58,9 +59,13 @@ module CommandT
       split_location = options[:match_window_at_top] ? 'topleft' : 'botright'
       if @@buffer # still have buffer from last time
         ::VIM::command "silent! #{split_location} #{@@buffer.number}sbuffer"
-        raise "Can't re-open GoToFile buffer" unless $curbuf.number == @@buffer.number
-        $curwin.height = 1
-      else        # creating match window for first time and set it up
+        if $curbuf.number != @@buffer.number
+          @@buffer = nil
+        else
+          $curwin.height = 1
+        end
+      end
+      if @@buffer.nil?        # creating match window for first time and set it up
         split_command = "silent! #{split_location} 1split GoToFile"
         [
           split_command,
@@ -249,16 +254,25 @@ module CommandT
     end
 
     def restore_window_dimensions
-      # sort from tallest to shortest
-      @windows.sort! { |a, b| b.height <=> a.height }
+      # sort from tallest to shortest, tie-breaking on window width
+      @windows.sort! do |a, b|
+        order = b.height <=> a.height
+        if order.zero?
+          b.width <=> a.width
+        else
+          order
+        end
+      end
 
       # starting with the tallest ensures that there are no constraints
       # preventing windows on the side of vertical splits from regaining
       # their original full size
       @windows.each do |w|
         # beware: window may be nil
-        window = ::VIM::Window[w.index]
-        window.height = w.height if window
+        if window = ::VIM::Window[w.index]
+          window.height = w.height
+          window.width  = w.width
+        end
       end
     end
 
@@ -339,20 +353,13 @@ module CommandT
     end
 
     def get_cursor_highlight
-      # as :highlight returns nothing and only prints,
-      # must redirect its output to a variable
-      ::VIM::command 'silent redir => g:command_t_cursor_highlight'
-
-      # force 0 verbosity to ensure origin information isn't printed as well
-      ::VIM::command 'silent! 0verbose highlight Cursor'
-      ::VIM::command 'silent redir END'
-
       # there are 3 possible formats to check for, each needing to be
       # transformed in a certain way in order to reapply the highlight:
       #   Cursor xxx guifg=bg guibg=fg      -> :hi! Cursor guifg=bg guibg=fg
       #   Cursor xxx links to SomethingElse -> :hi! link Cursor SomethingElse
       #   Cursor xxx cleared                -> :hi! clear Cursor
-      highlight = ::VIM::evaluate 'g:command_t_cursor_highlight'
+      highlight = VIM::capture 'silent! 0verbose highlight Cursor'
+
       if highlight =~ /^Cursor\s+xxx\s+links to (\w+)/
         "link Cursor #{$~[1]}"
       elsif highlight =~ /^Cursor\s+xxx\s+cleared/
