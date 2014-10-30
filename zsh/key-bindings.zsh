7c8133f514120253e56641d2a5589ba8271d55b1
Tobias Kraze <tobias.kraze@makandra.de>
2014-10-30 12:35:30 +0100

    zsh: fix ctrl-left and ctrl-right

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -7,5 +7,7 @@ source ~/.zkbd/$TERM
 [[ -n "${key[End]}"     ]]  && bindkey  "${key[End]}"     end-of-line
 [[ -n "${key[Insert]}"  ]]  && bindkey  "${key[Insert]}"  overwrite-mode
 [[ -n "${key[Delete]}"  ]]  && bindkey  "${key[Delete]}"  delete-char
+[[ -n "${key[C-Left]}"  ]]  && bindkey  "${key[C-Left]}"  emacs-backward-word
+[[ -n "${key[C-Right]}"  ]]  && bindkey  "${key[C-Right]}"  emacs-forward-word
 
 bindkey "^R" history-incremental-search-backward
